7c3dcd76511971822a1b556b860ba59e87fefebb
Acu1000 <140196544+Acu1000@users.noreply.github.com>
2023-11-18 19:16:31 +0100

    refactor: ShoulderCamera.cs overall

--- scripts/ShoulderCamera.cs
+++ scripts/ShoulderCamera.cs
@@ -4,26 +4,27 @@ using Cosmobot;
 
 public partial class ShoulderCamera : Camera3D
 {
-	[Export] public Vector3 Offset = new Vector3(1, 1, 2);
-	[Export] public float CameraSensitivity = 1.0f;
-	[Export] public float MaxVerticalAngleDegrees = 75f;
+	[Export] public Vector3 Offset { get; private set; } = new(1, 1, 2);
+	[Export] public float LookSensitivity { get; private set; } = 0.005f;
 
-	private float rotation_x;
-	private float rotation_y;
-	private Vector2 mouseDelta = Vector2.Zero;
+    [Export] private float _maxVerticalAngleDegrees = 75f;
+	public float MaxVerticalAngle {
+        get => Mathf.DegToRad(_maxVerticalAngleDegrees);
+        private set => _maxVerticalAngleDegrees = Mathf.RadToDeg(value);
+    }
+
+	private float _rotationX;
+	private float _rotationY;
 	
-	private float MaxVerticalAngle;
-	private Player player;
-	private RayCast3D clipPreventionRay;
-
+	private Player _player;
+	private RayCast3D _clipPreventionRay;
 
 	public override void _Ready()
 	{
-		player = (Player)GetParent();
-		clipPreventionRay = (RayCast3D)GetNode("ClipPreventionRay");
+		_player = (Player)GetParent();
+		_clipPreventionRay = (RayCast3D)GetNode("ClipPreventionRay");
 
-        clipPreventionRay.TargetPosition = Offset;
-        MaxVerticalAngle = Mathf.DegToRad(MaxVerticalAngleDegrees);
+        _clipPreventionRay.TargetPosition = Offset;
 
 		// Lock and hide the cursor
 		Input.MouseMode = Input.MouseModeEnum.Captured;
@@ -31,43 +32,32 @@ public partial class ShoulderCamera : Camera3D
 
 	public override void _Process(double delta)
 	{
-        // Set camera rotation
-        GlobalRotation = new Vector3(rotation_x, rotation_y, 0);
+        GlobalRotation = new(_rotationX, _rotationY, 0);
 
-        // Prevents camera from clipping into surroundings
-        clipPreventionRay.GlobalPosition = player.GlobalPosition;
-
-        clipPreventionRay.ForceRaycastUpdate();
-
-        // Calculate & set camera position
-        if (clipPreventionRay.IsColliding())  // There's an object in the way
+        _clipPreventionRay.GlobalPosition = _player.GlobalPosition;
+        _clipPreventionRay.ForceRaycastUpdate();
+        if (_clipPreventionRay.IsColliding()) // There's an object in the way
         {
             // (add a little extra safe distance from wall so the camera view won't partially clip inside)
-            GlobalPosition = clipPreventionRay.GetCollisionPoint() - Position.Normalized() * 0.1f;
+            GlobalPosition = _clipPreventionRay.GetCollisionPoint() - Position.Normalized() * 0.1f;
         }
         else // Can put camera at normal position
         {
             Vector3 effectiveOffset = Offset;
-            effectiveOffset = effectiveOffset.Rotated(Vector3.Right, rotation_x);
-            effectiveOffset = effectiveOffset.Rotated(Vector3.Up, rotation_y);
-            GlobalPosition = player.GlobalPosition + effectiveOffset;
+            effectiveOffset = effectiveOffset.Rotated(Vector3.Right, _rotationX);
+            effectiveOffset = effectiveOffset.Rotated(Vector3.Up, _rotationY);
+            GlobalPosition = _player.GlobalPosition + effectiveOffset;
         }
-
-        
     }
 
     public override void _Input(InputEvent @event)
     {
         if (@event is InputEventMouseMotion)
 		{
-			// Get mouse input and calculate new rotation
-			mouseDelta = ((InputEventMouseMotion)@event).Relative;
-            rotation_y -= mouseDelta.X * CameraSensitivity;
-            rotation_x -= mouseDelta.Y * CameraSensitivity;
-			rotation_x = Mathf.Clamp(rotation_x, -MaxVerticalAngle, MaxVerticalAngle);
-
-            // Reset mouse delta to zero because with no mouse movement _Input won't be called
-            mouseDelta = Vector2.Zero;
+			Vector2 _mouseDelta = ((InputEventMouseMotion)@event).Relative;
+            _rotationY -= _mouseDelta.X * LookSensitivity;
+            _rotationX -= _mouseDelta.Y * LookSensitivity;
+			_rotationX = Mathf.Clamp(_rotationX, -MaxVerticalAngle, MaxVerticalAngle);
         }
     }
 }

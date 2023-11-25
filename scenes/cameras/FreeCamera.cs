98f021ecbd4d2fde7fb3cebb7693df498cf75f47
Acu1000 <140196544+Acu1000@users.noreply.github.com>
2023-11-25 17:10:00 +0100

    style: fixed casing in FreeCamera.cs

--- scenes/cameras/FreeCamera.cs
+++ scenes/cameras/FreeCamera.cs
@@ -8,7 +8,7 @@ public partial class FreeCamera : CharacterBody3D
         GLOBAL,
         LOCAL
     }
-	[Export] public float moveSpeed { get; private set; } = 7.0f;
+	[Export] public float MoveSpeed { get; private set; } = 7.0f;
     [Export] private float _maxVerticalAngleDegrees = 75f;
     public float MaxVerticalAngle
     {
@@ -16,8 +16,8 @@ public partial class FreeCamera : CharacterBody3D
         private set => _maxVerticalAngleDegrees = Mathf.RadToDeg(value);
     }
     [Export] public float LookSensitivity { get; private set; } = 0.005f;
-    [Export] public CoordinateMode horizontalInputMode { get; private set; } = CoordinateMode.GLOBAL;
-    [Export] public CoordinateMode verticalInputMode { get; private set; } = CoordinateMode.GLOBAL;
+    [Export] public CoordinateMode HorizontalInputMode { get; private set; } = CoordinateMode.GLOBAL;
+    [Export] public CoordinateMode VerticalInputMode { get; private set; } = CoordinateMode.GLOBAL;
 
     public bool isActive = false; // TEMP
 
@@ -31,7 +31,6 @@ public partial class FreeCamera : CharacterBody3D
         _camera = (Camera3D)GetNode("Camera3D");
 	}
 
-	
 	public override void _Process(double delta)
 	{
         ProcessMovement((float)delta);
@@ -67,7 +66,7 @@ public partial class FreeCamera : CharacterBody3D
             inputVector += Transform.Basis.X * inputAxisX;
 
             float inputAxisZ = Input.GetAxis("move_up", "move_down");
-            if (horizontalInputMode == CoordinateMode.LOCAL)
+            if (HorizontalInputMode == CoordinateMode.LOCAL)
             {
                 inputVector += Transform.Basis.Z * inputAxisZ;
             }
@@ -77,7 +76,7 @@ public partial class FreeCamera : CharacterBody3D
             }
 
             float inputAxisY = Input.GetAxis("free_camera_down", "free_camera_up");
-            if (verticalInputMode == CoordinateMode.LOCAL)
+            if (VerticalInputMode == CoordinateMode.LOCAL)
             {
                 inputVector += Transform.Basis.Y * inputAxisY;
             }
@@ -87,7 +86,7 @@ public partial class FreeCamera : CharacterBody3D
             }
 
             inputVector = inputVector.Normalized();
-            Velocity = inputVector * moveSpeed;
+            Velocity = inputVector * MoveSpeed;
             MoveAndSlide();
         }
     }
